316af15ffd8145b842739f4553c511cffff15e1a
Renan Yamamoto <renanyfc07@gmail.com>
2026-02-16 19:31:07 -0300

    alteração na porta
    correção necessária para o deploy

--- routes.js
+++ routes.js
@@ -2,8 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("servidor iniciado");
 });
 
